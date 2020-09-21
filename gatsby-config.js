c2e5816c48973048d2bdee8f29074f8b295c3586
HiteshKumawat <hitesh.kumawat@loginradius.com>
2020-09-21 18:34:23 +0530

    hacktoberfest page UI

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,15 +84,15 @@ module.exports = {
               maxWidth: 768,
             },
           },
-          {
-            resolve: `gatsby-remark-vscode`,
-            options: {
-              theme: "Dark+ (default dark)",
-              languageAliases: {
-                html: "js",
-              },
-            },
-          },
+          // {
+          //   resolve: `gatsby-remark-vscode`,
+          //   options: {
+          //     theme: "Dark+ (default dark)",
+          //     languageAliases: {
+          //       html: "js",
+          //     },
+          //   },
+          // },
         ],
       },
     },
