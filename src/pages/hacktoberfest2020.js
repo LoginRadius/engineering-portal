4ab29a23f018e882b0670ea9c9544907f3eb6d72
Hridayesh Sharma <dev.hridayesh@gmail.com>
2020-09-23 16:32:34 +0530

    content improvements and added list of projects

--- src/pages/hacktoberfest2020.js
+++ src/pages/hacktoberfest2020.js
@@ -12,42 +12,51 @@ const Hacktoberfest = () => {
         <section className="content py-80">
           <div>
             <h2>
-              Hacktoberfest 2020 is here and{" "}
-              <span className="highlight">LoginRadius</span> welcomes it.
+              <span className="highlight">500 Swags</span> Are Waiting For You!
             </h2>
             <p>
               Hacktoberfest 2020 is around the corner and this year
               <span className="highlight"> LoginRadius </span> is welcoming
-              Hacktoberfest with open hands by open sourcing some of it's code
-              for people to contribute.
+              hacktoberfest with open hands by open sourcing many of it's
+              projects on GitHub for people to contribute on.
             </p>
             <p>
               Hacktoberfest is a month-long celebration of open-source software
               in October month run by DigitalOcean in partnership with GitHub
               and DEV. Hacktoberfest is open to everyone in our global
-              community!
+              community! Read more on{" "}
+              <a
+                target="_blank"
+                href="https://www.loginradius.com/engineering/blog/loginradius-supports-hacktoberfest-2020/"
+              >
+                LoginRadius supports hacktoberfest 2020
+              </a>
+              .
             </p>
-            <p className="highlight">
-              <em>
-                "Open 4 pull requests and win a limited edition t-shirt by
-                DigitalOcean."
-              </em>
-            </p>
-            <h3>LoginRadius is supporting Hacktoberfest 2020.</h3>
+
+            <h3>LoginRadius is supporting Hacktoberfest 2020</h3>
             <p>
-              Yes, this year LoginRadius will be supporting hacktoberfest by
-              open sourcing some of it's codebase. Check out the{" "}
-              <a href="">LoginRadius Github </a> for we will be providing
-              well-labeled issues for people to contribute. We will soon update
-              the list of our open source projects on our{" "}
-              <a href="/">Engineering Blog</a>.
+              Yes, this year LoginRadius will be part of hacktoberfest by open
+              sourcing many of it's projects on Github. We will be providing
+              well-labeled issues for anyone to contribute to our projects on
+              our GitHub Repositories. We will soon update the list of our open
+              source projects on our <a href="/">Engineering Blog</a>.
+            </p>
+            <h3>Why should you contribute?</h3>
+            <p>
+              Hacktoberfest is a great opportunity for anyone trying to get
+              started in open source and get involved in the community. It is
+              the easiest way to get into open source community and also show
+              off some cool swags which you get by contributing to lot of open
+              source projects.
             </p>
             <h3>Rewards from LoginRadius</h3>
             <p>
               Apart from getting limited edition t-shirts from DigitalOcean,
               LoginRadius will also provide it's{" "}
-              <span calssName="highlight">limited edition branded swags</span>
-              including <span className="highlight">t-shirts </span>, to people
+              <span calssName="highlight">limited edition branded swags </span>
+              including <span className="highlight">awesome t-shirts </span>,
+              and <span className="highlight">stickers</span> to people
               contributing to our codebase. Yes!{" "}
               <span className="highlight">
                 we have got around 500 swags waiting for you all.
@@ -71,7 +80,10 @@ const Hacktoberfest = () => {
                 form.
               </li>
             </ol>
-            <h3> How can you contribute to LoginRadius Open Sourc</h3>
+            <h3>
+              {" "}
+              How can you contribute to LoginRadius Open Source projects?
+            </h3>
             <p>
               We accept all kinds of contributions, be it improving our{" "}
               <a href="https://github.com/LoginRadius/docs">Docs</a>, writing
@@ -81,6 +93,33 @@ const Hacktoberfest = () => {
               </a>{" "}
               or contributing to our different open source projects on{" "}
               <a href="https://github.com/LoginRadius/">Github</a>.
+              <br />
+              Here is a list of some of our Projects on Github:
+              <ul>
+                <li>
+                  <a href="https://github.com/LoginRadius/engineering-portal">
+                    Engineering Blog
+                  </a>
+                </li>
+                <li>
+                  <a href="https://github.com/LoginRadius/docs">
+                    Developer Documentation
+                  </a>
+                </li>
+                <li>
+                  <a href="https://github.com/LoginRadius/go-saml">go-saml</a>
+                </li>
+                <li>
+                  <a href="https://github.com/LoginRadius/cascade">
+                    Developer Portal
+                  </a>
+                </li>
+              </ul>
+            </p>
+            <p>
+              Check out{" "}
+              <a href="https://github.com/LoginRadius">LoginRadius Github</a>{" "}
+              for more awesome projects.
             </p>
           </div>
         </section>
