37f825752b6139994d18d0956bd9ea55bdc66dda
Pranav <pranav@quze.co>
2020-09-29 23:32:38 +0530

    Add support for GIF Images in blog
    Added support for the various file like gif, pdf and many more other files.

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,6 +75,7 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          { resolve: "gatsby-remark-copy-linked-files" },
           {
             resolve: `gatsby-remark-relative-images`,
           },
