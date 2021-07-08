212fb6602167b69c63e6b81eff089dcbcfede091
Hiteshkrkumawat <hiteshkumawat19@gmail.com>
2021-07-08 10:57:31 +0530

    sendinblue widget UI

--- src/components/header.js
+++ src/components/header.js
@@ -13,7 +13,6 @@ import ReactGA from "react-ga"
 import { validEmail } from "./regex.js"
 import Helmet from "react-helmet"
 
-
 const logger = function (linkName, headerLink) {
   ReactGA.event({
     category: "Header Menu Clicks",
@@ -110,10 +109,10 @@ const Header = ({ menuLinks, searchIndex }) => {
         <script src="https://sibforms.com/forms/end-form/build/main.js"></script>
       </Helmet>
       <div
-        className={`${headerStyles.sgResponse} ${headerStyles[newsLetterSubscription.respClass]
-          } `}
+        className={`${headerStyles.sgResponse} ${
+          headerStyles[newsLetterSubscription.respClass]
+        } `}
       >
-
         <div>{newsLetterSubscription.responseMsg}</div>
         <button
           onClick={() =>
@@ -148,8 +147,9 @@ const Header = ({ menuLinks, searchIndex }) => {
       ) : null}
 
       <div
-        className={`${showMenu ? headerStyles.headerShowMenu : ""} ${headerStyles.header
-          }`}
+        className={`${showMenu ? headerStyles.headerShowMenu : ""} ${
+          headerStyles.header
+        }`}
       >
         <Link className={headerStyles.logo} to={"/"}>
           <img src={LogoLr} alt={`logo`} className={headerStyles.lrLogo} />
@@ -174,16 +174,18 @@ const Header = ({ menuLinks, searchIndex }) => {
         </div>
         <div className={headerStyles.bloglogoWrap}>
           <div
-            className={`${blogType ? headerStyles.allBlogActive : ""} ${headerStyles.allBlogsLogo
-              } `}
+            className={`${blogType ? headerStyles.allBlogActive : ""} ${
+              headerStyles.allBlogsLogo
+            } `}
             onMouseOver={() => (_shouldBlogClose = false)}
             onMouseLeave={() => (_shouldBlogClose = true)}
           >
             <div
-              className={`${["async", ""].includes(blogType)
-                ? headerStyles.active
-                : headerStyles.inactive
-                } ${headerStyles.logoWrap} ${headerStyles.async}`}
+              className={`${
+                ["async", ""].includes(blogType)
+                  ? headerStyles.active
+                  : headerStyles.inactive
+              } ${headerStyles.logoWrap} ${headerStyles.async}`}
               onClick={() => (blogType === "async" ? "" : toggleType("async"))}
             >
               <div className={headerStyles.blogLogo}>
@@ -192,17 +194,19 @@ const Header = ({ menuLinks, searchIndex }) => {
                 </div>
               </div>
               <div
-                className={`${blogType == "async"
-                  ? headerStyles.slideDesDown1
-                  : headerStyles.slideDesUp1
-                  }  ${headerStyles.description} ${headerStyles.async}`}
+                className={`${
+                  blogType == "async"
+                    ? headerStyles.slideDesDown1
+                    : headerStyles.slideDesUp1
+                }  ${headerStyles.description} ${headerStyles.async}`}
               >
                 <p>The latest news in the world of engineering.</p>
               </div>
             </div>
             <div
-              className={`${blogType == "swi" ? headerStyles.active : headerStyles.inactive
-                } ${headerStyles.logoWrap} ${headerStyles.swi} `}
+              className={`${
+                blogType == "swi" ? headerStyles.active : headerStyles.inactive
+              } ${headerStyles.logoWrap} ${headerStyles.swi} `}
               onClick={() => (blogType === "swi" ? "" : toggleType("swi"))}
             >
               <div className={headerStyles.blogLogo}>
@@ -211,10 +215,11 @@ const Header = ({ menuLinks, searchIndex }) => {
                 </div>
               </div>
               <div
-                className={`${blogType == "swi"
-                  ? headerStyles.slideDesDown1
-                  : headerStyles.slideDesUp1
-                  }  ${headerStyles.description} ${headerStyles.swi}`}
+                className={`${
+                  blogType == "swi"
+                    ? headerStyles.slideDesDown1
+                    : headerStyles.slideDesUp1
+                }  ${headerStyles.description} ${headerStyles.swi}`}
               >
                 <p>
                   Identity and Access Management (IAM), including security and
@@ -230,8 +235,9 @@ const Header = ({ menuLinks, searchIndex }) => {
               </div>
             </div>
             <div
-              className={`${blogType == "fuel" ? headerStyles.active : headerStyles.inactive
-                } ${headerStyles.logoWrap} ${headerStyles.fuel}`}
+              className={`${
+                blogType == "fuel" ? headerStyles.active : headerStyles.inactive
+              } ${headerStyles.logoWrap} ${headerStyles.fuel}`}
               onClick={() => (blogType === "fuel" ? "" : toggleType("fuel"))}
             >
               <div className={headerStyles.blogLogo}>
@@ -240,10 +246,11 @@ const Header = ({ menuLinks, searchIndex }) => {
                 </div>
               </div>
               <div
-                className={`${blogType == "fuel"
-                  ? headerStyles.slideDesDown1
-                  : headerStyles.slideDesUp1
-                  }  ${headerStyles.description} ${headerStyles.fuel}`}
+                className={`${
+                  blogType == "fuel"
+                    ? headerStyles.slideDesDown1
+                    : headerStyles.slideDesUp1
+                }  ${headerStyles.description} ${headerStyles.fuel}`}
               >
                 <p>
                   Grow your business to millions.Engage and retain your
@@ -269,8 +276,9 @@ const Header = ({ menuLinks, searchIndex }) => {
                     href={link.slug}
                     key={index}
                     target="_blank"
-                    className={`${link.class ? headerStyles[link.class] : ""
-                      } ga_event`}
+                    className={`${
+                      link.class ? headerStyles[link.class] : ""
+                    } ga_event`}
                     rel="noopener noreferrer"
                     onClick={() => logger(link.name, link.slug)}
                   >
@@ -313,78 +321,96 @@ const Header = ({ menuLinks, searchIndex }) => {
                 </form>
               </div> */}
 
-
-
-              <div class="sib-form">
-                <div id="sib-form-container" class="sib-form-container">
-                  <div id="error-message" class="sib-form-message-panel"
-                  >
-                    <div class="sib-form-message-panel__text sib-form-message-panel__text--center">
-                      <svg viewBox="0 0 512 512" class="sib-icon sib-notification__icon">
-                        <path
-                          d="M256 40c118.621 0 216 96.075 216 216 0 119.291-96.61 216-216 216-119.244 0-216-96.562-216-216 0-119.203 96.602-216 216-216m0-32C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm-11.49 120h22.979c6.823 0 12.274 5.682 11.99 12.5l-7 168c-.268 6.428-5.556 11.5-11.99 11.5h-8.979c-6.433 0-11.722-5.073-11.99-11.5l-7-168c-.283-6.818 5.167-12.5 11.99-12.5zM256 340c-15.464 0-28 12.536-28 28s12.536 28 28 28 28-12.536 28-28-12.536-28-28-28z" />
+              <div className="sib-form">
+                <div id="sib-form-container" className="sib-form-container">
+                  <div id="error-message" className="sib-form-message-panel">
+                    <div className="sib-form-message-panel__text sib-form-message-panel__text--center">
+                      <svg
+                        viewBox="0 0 512 512"
+                        className="sib-icon sib-notification__icon"
+                      >
+                        <path d="M256 40c118.621 0 216 96.075 216 216 0 119.291-96.61 216-216 216-119.244 0-216-96.562-216-216 0-119.203 96.602-216 216-216m0-32C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm-11.49 120h22.979c6.823 0 12.274 5.682 11.99 12.5l-7 168c-.268 6.428-5.556 11.5-11.99 11.5h-8.979c-6.433 0-11.722-5.073-11.99-11.5l-7-168c-.283-6.818 5.167-12.5 11.99-12.5zM256 340c-15.464 0-28 12.536-28 28s12.536 28 28 28 28-12.536 28-28-12.536-28-28-28z" />
                       </svg>
-                      <span class="sib-form-message-panel__inner-text">
+                      <span className="sib-form-message-panel__inner-text">
                         Your subscription could not be saved. Please try again.
                       </span>
                     </div>
                   </div>
                   <div></div>
-                  <div id="success-message" class="sib-form-message-panel">
-                    <div class="sib-form-message-panel__text sib-form-message-panel__text--center">
-                      <svg viewBox="0 0 512 512" class="sib-icon sib-notification__icon">
-                        <path
-                          d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 464c-118.664 0-216-96.055-216-216 0-118.663 96.055-216 216-216 118.664 0 216 96.055 216 216 0 118.663-96.055 216-216 216zm141.63-274.961L217.15 376.071c-4.705 4.667-12.303 4.637-16.97-.068l-85.878-86.572c-4.667-4.705-4.637-12.303.068-16.97l8.52-8.451c4.705-4.667 12.303-4.637 16.97.068l68.976 69.533 163.441-162.13c4.705-4.667 12.303-4.637 16.97.068l8.451 8.52c4.668 4.705 4.637 12.303-.068 16.97z" />
+                  <div id="success-message" className="sib-form-message-panel">
+                    <div className="sib-form-message-panel__text sib-form-message-panel__text--center">
+                      <svg
+                        viewBox="0 0 512 512"
+                        className="sib-icon sib-notification__icon"
+                      >
+                        <path d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 464c-118.664 0-216-96.055-216-216 0-118.663 96.055-216 216-216 118.664 0 216 96.055 216 216 0 118.663-96.055 216-216 216zm141.63-274.961L217.15 376.071c-4.705 4.667-12.303 4.637-16.97-.068l-85.878-86.572c-4.667-4.705-4.637-12.303.068-16.97l8.52-8.451c4.705-4.667 12.303-4.637 16.97.068l68.976 69.533 163.441-162.13c4.705-4.667 12.303-4.637 16.97.068l8.451 8.52c4.668 4.705 4.637 12.303-.068 16.97z" />
                       </svg>
-                      <span class="sib-form-message-panel__inner-text">
+                      <span className="sib-form-message-panel__inner-text">
                         Please verify your email address.
                       </span>
                     </div>
                   </div>
                   <div></div>
-                  <div id="sib-container" class="sib-container--large sib-container--vertical">
-                    <form id="sib-form" method="POST"
+                  <div
+                    id="sib-container"
+                    className="sib-container--large sib-container--vertical"
+                  >
+                    <form
+                      id="sib-form"
+                      method="POST"
                       action="https://7b214b8d.sibforms.com/serve/MUIEABjlbtas8SGeh1_RHkqf-_rjMNzQ_3u_4maezMOZVA-Y8EhuES3-7h1h1an4yYoFbmXE-yi_3mvlfauUKpZxhhpOfH-eEcDiwn1SFnCLVyXROs6Z1Qiz6-_7-Bi-3cGVPJgdXXUuWgo2nQXMnkCl7NiAhIO1lUCHGg6EPo6jH1MahkllNh1mJtf4HeL-sQy6fDXP7WdtwJbA"
-                      data-type="subscription">
+                      data-type="subscription"
+                    >
                       <div>
-                        <div class="sib-input sib-form-block">
-                          <div class="form__entry entry_block">
-                            <div class="form__label-row ">
-
-                              <div class="entry__field">
-                                <input class="input" type="text" id="EMAIL" name="EMAIL" autocomplete="off"
-                                  placeholder="you@example.com" data-required="true" required />
+                        <div className="sib-input sib-form-block">
+                          <div className="form__entry entry_block">
+                            <div className="form__label-row ">
+                              <div className="entry__field">
+                                <input
+                                  className="input"
+                                  type="text"
+                                  id="EMAIL"
+                                  name="EMAIL"
+                                  autocomplete="off"
+                                  placeholder="you@example.com"
+                                  data-required="true"
+                                  required
+                                />
                               </div>
                             </div>
 
-                            <label class="entry__error entry__error--primary">
-                            </label>
+                            <label className="entry__error entry__error--primary"></label>
                           </div>
                         </div>
                       </div>
                       <div>
-                        <div class="sib-form-block">
-                          <button class="sib-form-block__button sib-form-block__button-with-loader"
-                            form="sib-form" type="submit">
-                            <svg class="icon clickable__icon progress-indicator__icon sib-hide-loader-icon"
-                              viewBox="0 0 512 512">
-                              <path
-                                d="M460.116 373.846l-20.823-12.022c-5.541-3.199-7.54-10.159-4.663-15.874 30.137-59.886 28.343-131.652-5.386-189.946-33.641-58.394-94.896-95.833-161.827-99.676C261.028 55.961 256 50.751 256 44.352V20.309c0-6.904 5.808-12.337 12.703-11.982 83.556 4.306 160.163 50.864 202.11 123.677 42.063 72.696 44.079 162.316 6.031 236.832-3.14 6.148-10.75 8.461-16.728 5.01z" />
+                        <div className="sib-form-block">
+                          <button
+                            className={`${headerStyles.navcta} ${headerStyles.sgSubmitBtn} btn-primary sib-form-block__button sib-form-block__button-with-loader`}
+                            form="sib-form"
+                            type="submit"
+                          >
+                            <svg
+                              className="icon clickable__icon progress-indicator__icon sib-hide-loader-icon"
+                              viewBox="0 0 512 512"
+                            >
+                              <path d="M460.116 373.846l-20.823-12.022c-5.541-3.199-7.54-10.159-4.663-15.874 30.137-59.886 28.343-131.652-5.386-189.946-33.641-58.394-94.896-95.833-161.827-99.676C261.028 55.961 256 50.751 256 44.352V20.309c0-6.904 5.808-12.337 12.703-11.982 83.556 4.306 160.163 50.864 202.11 123.677 42.063 72.696 44.079 162.316 6.031 236.832-3.14 6.148-10.75 8.461-16.728 5.01z" />
                             </svg>
                             SUBSCRIBE
                           </button>
                         </div>
                       </div>
-                      <input type="text" name="email_address_check" value="" class="input--hidden" />
+                      <input
+                        type="text"
+                        name="email_address_check"
+                        value=""
+                        className="input--hidden"
+                      />
                       <input type="hidden" name="locale" value="en" />
                     </form>
                   </div>
                 </div>
               </div>
-
-
-
-
             </div>
           </div>
         </div>
