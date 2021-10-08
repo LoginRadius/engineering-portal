2fc1f636a6e39617fbaf1c53ac97d2440b891de4
pompodars <sergey@webapps.studio>
2021-10-08 22:18:56 +0300

    TOC-fix-timeout

--- src/components/toc__hyperlinkNavFloating.js
+++ src/components/toc__hyperlinkNavFloating.js
@@ -2,7 +2,6 @@ var elementToClickOnID = "tocTrigger";
 var elementToChangeId = "tocUl";
 
 var slideOutName = "initialValue";
-var elementToChangeClassList = "initialValue";
 
 var gatsbyWrapperID = "gatsby-focus-wrapper";
 
@@ -12,80 +11,95 @@ var media1620on = false;
 var tocShown = true;
 
 
-window.onload = function () {
+// window.onload = function()
+// {
 
-    window.onscroll = function () {
-        doSomethingAtStart();
-    };
+window.onscroll = function() {
+    doSomethingAtStart();
+};
 
-    window.onresize = function ()
-    {
-        checkMedia( media1620 );
-
-        ( checkForElement() && media1620on )
-            ? hideTOC()
-            : showTOC();
-    };
-
-    setTimeout(function () {
-        doSomethingAtStart();
-    }, 2000);
-
-
-    function doSomethingAtStart() {
-
-        if ( !checkForElement() ) return;
-
-        var elementToChangeId = "tocUl";
-        var elementToChangeClassList = document.getElementById(elementToChangeId).classList;
-
-        for (var i = 0; i < elementToChangeClassList.length; i++) {
-            if (elementToChangeClassList[i].includes("slideOut")) {
-                slideOutName = elementToChangeClassList[i];
-            }
-        }
-
-        if ( media1620on ) hideTOC();
-    }
-
-    function hideTOC() {
-        document.getElementById(elementToChangeId).classList.remove(slideOutName);
-        tocShown = false;
-    }
-
-    function showTOC() {
-        document.getElementById(elementToChangeId).classList.add(slideOutName);
-        tocShown = true;
-    }
-
-
-    document.addEventListener('click', function (event)
-    {
-        // If the clicked element doesn't have the right selector, bail
-        if ( !event.target.matches("#" + elementToClickOnID) ) return;
-
-        // Don't follow the link
-        event.preventDefault();
-
-        ( checkForElement() && tocShown )
-            ? hideTOC()
-            : showTOC();
-
-    }, false);
-
-
-    function checkForElement() {
-        return ( document.getElementById(elementToChangeId ) != null );
-    }
-
-    // Call listener function at run time
+window.onresize = function ()
+{
     checkMedia( media1620 );
+
+    ( checkForElement() && media1620on )
+        ? hideTOC()
+        : showTOC();
+};
+
+window.onclick = function() {
+    timeoutDoSomething();
 }
+
+
+timeoutDoSomething();
+
+function timeoutDoSomething()
+{
+    setTimeout(function()
+    {
+        checkForElement()
+            ? doSomethingAtStart()
+            : timeoutDoSomething();
+
+    }, 2000);
+}
+
+
+
+
+
+function doSomethingAtStart()
+{
+    if ( !checkForElement() ) return;
+
+    var elementToChangeClassList = document.getElementById(elementToChangeId).classList;
+
+    for (var i = 0; i < elementToChangeClassList.length; i++) {
+        if (elementToChangeClassList[i].includes("slideOut")) {
+            slideOutName = elementToChangeClassList[i];
+        }
+    }
+
+    if ( media1620on ) hideTOC();
+}
+
+function hideTOC() {
+    document.getElementById(elementToChangeId).classList.remove(slideOutName);
+    tocShown = false;
+}
+
+function showTOC() {
+    document.getElementById(elementToChangeId).classList.add(slideOutName);
+    tocShown = true;
+}
+
+document.addEventListener('click', function (event)
+{
+    // If the clicked element doesn't have the right selector, bail
+    if ( !event.target.matches("#" + elementToClickOnID) ) return;
+
+    // Don't follow the link
+    event.preventDefault();
+
+    ( checkForElement() && tocShown )
+        ? hideTOC()
+        : showTOC();
+
+}, false);
+
+
+function checkForElement() {
+    return ( document.getElementById(elementToChangeId ) != null );
+}
+
+// Call listener function at run time
+checkMedia( media1620 );
+// }
 
 
 function checkMedia( media1620 )
 {
     // If media query matches
     media1620on = media1620.matches;
-    console.log(media1620on);
 }
