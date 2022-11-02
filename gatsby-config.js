93f1db5d239abf2d665132e4804b5e5f98e98ac2
kailashloginradius <110532206+kailashloginradius@users.noreply.github.com>
2022-11-02 13:19:00 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,8 @@ module.exports = {
   siteMetadata: {
     title: `LoginRadius Blog`,
     description: "Company Updates, Technology Articles from LoginRadius",
-    siteUrl: "https://www.loginradius.com",
-    feedUrl: "https://www.loginradius.com/blog",
+    siteUrl: "https://blog.loginradius.com",
+    feedUrl: "https://blog.loginradius.com",
     image: "/async.svg",
     owner: "LoginRadius",
     footerLinks: [
